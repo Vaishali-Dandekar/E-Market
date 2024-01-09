a21205185fbf68189f3996eebe50d0f5c676bbf0
Vaishali-Dandekar <vaishali.dandekar23@gmail.com>
2024-01-09 14:55:15 +0530

    first commit

---
+++ src/main/java/com/jbk/service/SupplierService.java
@@ -0,0 +1,16 @@
+package com.jbk.service;
+
+import java.util.List;
+
+
+import com.jbk.model.SupplierModel;
+
+
+public interface SupplierService {
+	public int addSupplier(SupplierModel supplierModel);
+	public SupplierModel getSupplierById(long supplierId);
+	public List<SupplierModel> getAllSuppliers();
+	public void updateSupplier(SupplierModel supplierModel);
+	public void deleteSupplier(long supplierId);
+	public List<SupplierModel> getSupplierByName(String supplierName);
+}
